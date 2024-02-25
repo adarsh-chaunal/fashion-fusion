da7b6ada1de0872d7af411b5f3691f962c6154df
adarsh-chaunal <adarsh97@gmail.com>
2024-02-25 19:21:27 +0530

    Configure firebase for authentication using google.

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,66 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"; // create an app instance based on config given below
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCxWrJb2PR6BjUkAKtuh4iFlUI6xMcioIQ",
+  authDomain: "fashion-fusion-db.firebaseapp.com",
+  projectId: "fashion-fusion-db",
+  storageBucket: "fashion-fusion-db.appspot.com",
+  messagingSenderId: "188443003804",
+  appId: "1:188443003804:web:9363a015f5ae460e635659",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, "users", userAuth.uid );
+    
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+
+    console.log(userSnapshot);
+    console.log(userSnapshot.exists());
+
+    if(!userSnapshot.exists()){
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try{ // create a new user
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        }
+        catch(error){
+            console.log('error creating the user', error.message);
+        }
+    }
+
+    return userDocRef;
+};
